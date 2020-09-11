7e1a107d9c5c03f3de33c5e3d8bf8dba935d805d
Haruto Sato <k097dynasty@icloud.com>
2020-09-11 09:37:00 +0900

    added pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,6 +55,7 @@ export default {
   modules: [
     // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
     '@nuxtjs/bulma',
+    '@nuxtjs/pwa',
   ],
   /*
    ** Build configuration
