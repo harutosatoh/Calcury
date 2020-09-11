e0dd531863182122f8349ae7f593900b31adefb5
Haruto Sato <k097dynasty@icloud.com>
2020-09-11 10:58:17 +0900

    added components

--- pages/index.vue
+++ pages/index.vue
@@ -22,6 +22,102 @@
         </a>
       </div>
     </div>
+    <div id="input-container">
+      <div class="p-title-container">
+        <p>参加者の名前を入れましょう</p>
+      </div>
+      <div class="field">
+        <div class="control">
+          <input
+            class="input is-warning"
+            type="text"
+            placeholder="一人目の名前を入力してください"
+          />
+        </div>
+      </div>
+      <div class="field">
+        <div class="control">
+          <input
+            class="input is-info"
+            type="text"
+            placeholder="二人目の名前を入力してください"
+          />
+        </div>
+      </div>
+      <div class="field">
+        <div class="control">
+          <input
+            class="input is-primary"
+            type="text"
+            placeholder="三人目の名前を入力してください"
+          />
+        </div>
+      </div>
+    </div>
+    <div id="num-input-container">
+      <div class="p-title-container">
+        <p>割り勘する金額を入れてください</p>
+      </div>
+    </div>
+    <div id="table">
+      <div class="p-title-container">
+        <p>現在一人当たりの金額は...</p>
+      </div>
+      <div class="card">
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <img
+                  src="https://picsum.photos/id/237/200/300"
+                  alt="Placeholder image"
+                />
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">￥2888</p>
+              <p class="subtitle is-6">@johnsmith</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <img
+                  src="https://picsum.photos/200/300?grayscale"
+                  alt="Placeholder image"
+                />
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">￥2888</p>
+              <p class="subtitle is-6">@johnsmith</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <img
+                  src="https://picsum.photos/seed/picsum/200/300"
+                  alt="Placeholder image"
+                />
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">￥2888</p>
+              <p class="subtitle is-6">@johnsmith</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,10 +128,9 @@ export default {}
 <style>
 .container {
   margin: 0 auto;
+  padding-left: 30px;
+  padding-right: 30px;
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   text-align: center;
 }
 
@@ -43,8 +138,8 @@ export default {}
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
-  font-weight: 300;
-  font-size: 100px;
+  font-weight: 400;
+  font-size: 35px;
   color: #35495e;
   letter-spacing: 1px;
 }
@@ -54,10 +149,16 @@ export default {}
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;
-  padding-bottom: 15px;
+  padding-bottom: 35px;
 }
 
 .links {
   padding-top: 15px;
+  display: none;
+}
+
+.p-title-container {
+  margin-top: 30px;
+  margin-bottom: 20px;
 }
 </style>
