38761f91838e6c738382f27f6b1c39395927f6af
Haruto Sato <k097dynasty@icloud.com>
2020-09-22 13:31:18 +0900

    modified design1

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="container">
-    <div id="banner">
-      <p>このページをホーム画面に追加することで、通常のアプリとして使用できるようになります！</p>
-    </div>
+    <article class="message is-success">
+      <div class="message-header">
+        <p>Notice</p>
+        <button class="delete" aria-label="delete"></button>
+      </div>
+      <div class="message-body" style="font-size: 0.9rem;">
+       このページをホーム画面に追加することで、アプリとしてお使いいただくことができます。
+      </div>
+    </article>
     <div class="header">
-      <img src="../static/icon.png" alt="">
+      <img src="../static/icon.png" alt="" />
       <h1 class="title">Calcury</h1>
-      <div class='desc'>
-        <p>Calcuryは3人グループでのちょっとしたお金の管理を助けるアプリです。合計金額やそれぞれが払った額を記録しておき、あとでしっかり確認できるようにしましょう！</p>
+      <div class="desc">
+        <p>
+          Calcuryは3人グループでのちょっとしたお金の管理を助けるアプリです。合計金額やそれぞれが払った額を記録しておき、あとでしっかり確認できるようにしましょう！
+        </p>
       </div>
       <div class="links">
         <a
@@ -43,7 +51,13 @@
           />
         </div>
       </div>
-      <button class="button is-success is-rounded" v-bind:disabled="isPush1" @click="insertUser1">追加</button>
+      <button
+        class="button is-success is-rounded"
+        v-bind:disabled="isPush1"
+        @click="insertUser1"
+      >
+        追加
+      </button>
       <div class="field">
         <div class="control">
           <input
@@ -55,7 +69,13 @@
           />
         </div>
       </div>
-      <button class="button is-success is-rounded" v-bind:disabled="isPush2" @click="insertUser2">追加</button>
+      <button
+        class="button is-success is-rounded"
+        v-bind:disabled="isPush2"
+        @click="insertUser2"
+      >
+        追加
+      </button>
       <div class="field">
         <div class="control">
           <input
@@ -67,7 +87,13 @@
           />
         </div>
       </div>
-      <button class="button is-success is-rounded" v-bind:disabled="isPush3" @click="insertUser3">追加</button>
+      <button
+        class="button is-success is-rounded"
+        v-bind:disabled="isPush3"
+        @click="insertUser3"
+      >
+        追加
+      </button>
     </div>
     <div id="num-input-container">
       <div class="p-title-container">
@@ -89,48 +115,61 @@
           placeholder="内容を入力してください"
         />
       </div>
-      <button class="button is-primary is-focused" @click="insert">追加</button>
+      <button class="button is-primary is-focused is-rounded" @click="insert">追加</button>
     </div>
     <div id="table">
       <div class="panel is-primary">
-        <tr class="panel-block is-active" v-for="(item, index) in payments" :key="index">
+        <tr
+          class="panel-block is-active"
+          v-for="(item, index) in payments"
+          :key="index"
+        >
           <td class="num">￥{{ item.number }}</td>
           <td class="content">{{ item.content }}</td>
-      </tr>
+        </tr>
       </div>
       <div class="p-title-container">
         <p class="show-sum">現在の合計:</p>
         <h3 class="sum">￥{{ this.$store.state.currentSum[0].number }}</h3>
         <p class="show-sum">一人あたりの金額（小数点以下切り捨て):</p>
-        <h3 class="sum">￥{{ divider(this.$store.state.currentSum[0].number)}}</h3>
+        <h3 class="sum">
+          ￥{{ divider(this.$store.state.currentSum[0].number) }}
+        </h3>
       </div>
       <div v-for="(item, index) in users" :key="index">
-      <div class="card">
-        <div class="card-content">
-          <div class="media">
-            <div class="media-left">
-              <figure class="image is-48x48">
-                <img
-                  src="https://picsum.photos/id/237/200/300"
-                  alt="Placeholder image"
-                />
-              </figure>
-            </div>
-            <div class="media-content">
-              <p class="subtitle is-6">{{ item.name }}</p>
-              <p>支払済</p>
-              <p class="title is-4">￥{{ $store.state.paymentsEach[item.uid].number }}</p>
+        <div class="card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img
+                    src="https://picsum.photos/id/237/200/300"
+                    alt="Placeholder image"
+                  />
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="subtitle is-6">{{ item.name }}</p>
+                <p>支払済</p>
+                <p class="title is-4">
+                  ￥{{ $store.state.paymentsEach[item.uid].number }}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
           <input
             class="input is-primary card-content"
             type="number"
             v-model="paymentseach"
             placeholder="個別に支払った金額を入れてください"
           />
-          <button class="button is-primary is-light card-content" @click="insertPayment(item.uid)">支払いを追加</button>
-      </div>
+          <button
+            class="button is-primary is-light card-content is-rounded"
+            @click="insertPayment(item.uid)"
+          >
+            支払いを追加
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -142,7 +181,7 @@ export default {
   namespaced: true,
   data: () => {
     return {
-      isPush : false,
+      isPush: false,
     }
   },
   data: () => {
@@ -154,31 +193,43 @@ export default {
     ...mapState(['payments']),
     ...mapState(['users']),
     ...mapState(['currentSum']),
-    ...mapState(['paymentsEach'])
+    ...mapState(['paymentsEach']),
   },
   methods: {
     insertUser1: function () {
-      console.log("called")
+      console.log('called')
       if (this.first !== '') {
-        this.$store.commit('insertUser', { name: this.first, uid: 1, payment: '0' }, { root: true })
+        this.$store.commit(
+          'insertUser',
+          { name: this.first, uid: 1, payment: '0' },
+          { root: true }
+        )
         console.log(this.first)
         this.isPush1 = true
         this.first = ''
       }
     },
     insertUser2: function () {
-      console.log("called")
+      console.log('called')
       if (this.second !== '') {
-        this.$store.commit('insertUser', { name: this.second, uid: 2, payment: '0' }, { root: true })
+        this.$store.commit(
+          'insertUser',
+          { name: this.second, uid: 2, payment: '0' },
+          { root: true }
+        )
         console.log(this.second)
         this.isPush2 = true
         this.second = ''
       }
     },
     insertUser3: function () {
-      console.log("called")
+      console.log('called')
       if (this.third !== '') {
-        this.$store.commit('insertUser', { name: this.third, uid: 3, payment: '0' }, { root: true })
+        this.$store.commit(
+          'insertUser',
+          { name: this.third, uid: 3, payment: '0' },
+          { root: true }
+        )
         console.log(this.first)
         this.isPush3 = true
         this.third = ''
@@ -187,28 +238,36 @@ export default {
     insert: function () {
       if (this.content !== '' && this.sum !== null) {
         var currentSum = this.$store.state.currentSum[0].number
-        var currentSumNum = parseInt( currentSum, '10')
-        var sumNum = parseInt( this.sum, '10')
-        console.log( currentSum )
+        var currentSumNum = parseInt(currentSum, '10')
+        var sumNum = parseInt(this.sum, '10')
+        console.log(currentSum)
         var totalSum = sumNum + currentSumNum
-        this.$store.commit('insert', { number: this.sum, content: this.content }, { root: true })
+        this.$store.commit(
+          'insert',
+          { number: this.sum, content: this.content },
+          { root: true }
+        )
         this.$store.commit('insertSum', { totalSum: totalSum }, { root: true })
-        this.sum= ''
+        this.sum = ''
         this.content = ''
       }
     },
     insertPayment: function (uid) {
-      this.$store.commit('paymentsEach', { number: this.paymentseach, uid: uid }, { root: true })
+      this.$store.commit(
+        'paymentsEach',
+        { number: this.paymentseach, uid: uid },
+        { root: true }
+      )
       this.paymentseach = ''
       console.log(this.paymentseach)
     },
     divider: function (number) {
       var num = parseInt(number, '10')
-      var numDivided = num/3
+      var numDivided = num / 3
       var numInt = Math.floor(numDivided)
       return numInt
-    }
-  }
+    },
+  },
 }
 </script>
 
